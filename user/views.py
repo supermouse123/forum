1a8f66337f58bc458ccf8bd547c7b005bd27699d
sunyingqiang <sunyingqiang@mosesceo.com>
2019-08-22 09:44:53 +0800

    增加了user模块

--- user/views.py
+++ user/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def user_model(request):
+    return HttpResponse("this is user")
 
